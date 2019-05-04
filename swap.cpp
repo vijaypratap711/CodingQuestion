e7d84d539d1e3135137960ceadce1272b00acefc
vijaypratap711 <vijayraj7111996@gmail.com>
2019-05-04 16:13:22 +0530

    Swap Numbers

---
+++ swap.cpp
@@ -0,0 +1,27 @@
+/**************************************************
+*_____Vijay Pratap Singh__________________________*
+*_____Computer Science & Engineering______________*
+*_____National Institute of Technology Agartala___*
+*_____Mail: vps711@outlook.com____________________*
+***************************************************
+Question: 
+
+**************************************************/
+#include<bits/stdc++.h>
+using namespace std;
+void swapNumbers(int &x,int &y){
+	x=x+y;
+	y=x-y;
+	x=x-y;
+}
+int main(){
+	int a,b;
+	a=2;
+	b=3;
+	cout<<"Before Swap\n";
+	cout<<"a: "<<a<<"\nb: "<<b;
+	swapNumbers(a,b);
+	cout<<"\nAfter Swap\n";
+	cout<<"a: "<<a<<"\nb: "<<b<<"\n";
+	return 0;
+}
