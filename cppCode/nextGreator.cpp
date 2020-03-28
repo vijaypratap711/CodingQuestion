1eb9dbb8191497ac8c30c7be2e2f6cce81097324
Vijay Pratap <vijayraj7111996@gmail.com>
2020-03-28 15:18:29 +0530

    new Promgram

---
+++ cppCode/nextGreator.cpp
@@ -0,0 +1,32 @@
+/**************************************************
+*_____Vijay Pratap Singh__________________________*
+*_____Computer Science & Engineering______________*
+*_____National Institute of Technology Agartala___*
+*_____Mail: vps711@outlook.com____________________*
+***************************************************
+Question: https://www.geeksforgeeks.org/find-next-greater-number-set-digits/
+Find next greater number with same set of digits
+Given a number n, find the smallest number that has same set of digits as n 
+and is greater than n. If n is the greatest possible number with its set of 
+digits, then print “not possible”.
+Examples:
+For simplicity of implementation, we have considered input number as a string.
+
+Input:  n = "218765"
+Output: "251678"
+
+Input:  n = "1234"
+Output: "1243"
+
+Input: n = "4321"
+Output: "Not Possible"
+
+Input: n = "534976"
+Output: "536479"
+**************************************************/
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+	return 0;
+}
